e0167c13b0befb35eeca60579ef3b726e89a7afb
Ricardo J. Mendez <ricardo@numergent.com>
2016-12-17 14:05:04 +0200

    Bringing comments up to standards

--- src/machtest/db.cljs
+++ src/machtest/db.cljs
@@ -38,9 +38,9 @@
 (def pg-types (.-types pg))
 
 
-; Going to configure type coercion using node-pg-types (https://github.com/brianc/node-pg-types)
-;
-; This depends on Postgres' data type ids: https://doxygen.postgresql.org/include_2catalog_2pg__type_8h.html
+;; Going to configure type coercion using node-pg-types (https://github.com/brianc/node-pg-types)
+;;
+;; This depends on Postgres' data type ids: https://doxygen.postgresql.org/include_2catalog_2pg__type_8h.html
 
 (def TYPANALYZE 20)
 (.setTypeParser
@@ -48,21 +48,21 @@
   TYPANALYZE
   (fn [val]
     (let [as-float (js/parseFloat val)]
-      ; The reason I need to do this now is that querying for a "count" is
-      ; returning a string. Looking at the metadata it's a data type of 20,
-      ; which is "analyze".
-      ;
-      ; https://doxygen.postgresql.org/include_2catalog_2pg__type_8h.html#ab5abe002baf3cb0ccf3f98008c72ca8a
-      ;
-      ; Javascript numeric parsing is wonky, though.
-      ;
-      ; (js/parseInt "8Ricardo") is 8.
-      ; (js/parseFloat "8.5.9Tomato") is 8.5
-      ;
-      ; ಠ_ಠ
-      ;
-      ; Hopefully we won't get back any TYPANALYZE result that starts with a
-      ; number but is not actually numeric. Requires more experimentation.
+      ;; The reason I need to do this now is that querying for a "count" is
+      ;; returning a string. Looking at the metadata it's a data type of 20,
+      ;; which is "analyze".
+      ;;
+      ;; https://doxygen.postgresql.org/include_2catalog_2pg__type_8h.html#ab5abe002baf3cb0ccf3f98008c72ca8a
+      ;;
+      ;; Javascript numeric parsing is wonky, though.
+      ;;
+      ;; (js/parseInt "8Ricardo") is 8.
+      ;; (js/parseFloat "8.5.9Tomato") is 8.5
+      ;;
+      ;; ಠ_ಠ
+      ;;
+      ;; Hopefully we won't get back any TYPANALYZE result that starts with a
+      ;; number but is not actually numeric. Requires more experimentation.
       (cond
         (nil? val) nil
         (js/isNaN as-float) val
@@ -92,10 +92,10 @@
   back in case of an exception. Any exception caught will be re-thrown."
   [f]
   (let [client   (wait (.connect @db-pool))
-        ; Wrapping the entire client causes an issue on repeated requests,
-        ; because it loses some references. Looks like what we get back from
-        ; the wrapping is a proxy. It's probably meant for wrapping modules
-        ; and not instances, but wrapping a single function works well.
+        ;; Wrapping the entire client causes an issue on repeated requests,
+        ;; because it loses some references. Looks like what we get back from
+        ;; the wrapping is a proxy. It's probably meant for wrapping modules
+        ;; and not instances, but wrapping a single function works well.
         q-future (wrap-future client.query)
         query    (fn [qs & rest]
                    (wait (.call q-future client qs (clj->js rest))))]
